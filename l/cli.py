b85b456da6e245f10f3fbc4252dd2d635d4a7efc
Julian Berman <Julian@GrayVines.com>
2015-10-05 16:41:08 -0400

    Make it work.

--- l/cli.py
+++ l/cli.py
@@ -4,6 +4,17 @@ from l.core import _INCLUDING_DOT_AND_DOTDOT, ls
 from l.project import project
 
 
+VARIADIC = -1  # click uses -1 for nargs which is very hard to read
+
+
+class Project(click.ParamType):
+    name = "project"
+    convert = lambda self, value, param, ctx : project(value)
+
+
+PROJECT = Project()
+
+
 @click.command()
 @click.option(
     "-a", "--all",
@@ -12,14 +23,21 @@ from l.project import project
 )
 @click.option(
     "-A", "--almost-all", "all",
-    flag_value=True,
+    flag_value="almost",
     help="Do not ignore entries that start with '.'",
 )
-@click.argument("files", nargs=-1)
+@click.argument(
+    "files",
+    nargs=VARIADIC,
+    type=PROJECT,
+)
 def run(all, files):
     """
     Project-oriented directory and file information lister.
 
     """
 
-    ls(files=files, show_all=all)
+    ls(
+        files=files or (project("."),),
+        show_all=True if all == "almost" else all,
+    )
