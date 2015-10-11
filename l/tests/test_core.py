7bd8a47be85ff7a02d1fa6692d314a1fa45540cc
Julian Berman <Julian@GrayVines.com>
2015-10-11 11:29:30 -0400

    Remove this for now, it's useless.

--- l/tests/test_core.py
+++ l/tests/test_core.py
@@ -3,23 +3,7 @@ from unittest import TestCase
 
 from bp.memory import MemoryFS, MemoryPath
 
-from l.core import ls, show
-
-
-class TestLS(TestCase):
-    def setUp(self):
-        self.fs = MemoryFS()
-        self.root = MemoryPath(fs=self.fs, path=("test-dir",))
-        self.root.createDirectory()
-
-    def test_it_lists_directories(self):
-        one, two = self.root.child("one"), self.root.child("two")
-        one.setContent("")
-        two.setContent("")
-
-        files = ls(path=self.root)
-
-        self.assertEqual(files, [one, two])
+from l import core
 
 
 class TestShow(TestCase):
@@ -30,7 +14,7 @@ class TestShow(TestCase):
 
     def assertShows(self, result, **kwargs):
         self.assertEqual(
-            show(**kwargs),
+            core.show(**kwargs),
             dedent(result).strip("\n") + "\n",
         )
 
