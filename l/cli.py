4ece764348e886c6b081b364fb4010b7e4043185
Julian Berman <Julian@GrayVines.com>
2015-10-11 11:12:14 -0400

    Move.

--- l/cli.py
+++ l/cli.py
@@ -2,7 +2,7 @@ from sys import stdout
 
 import click
 
-from l.core import _INCLUDING_DOT_AND_DOTDOT, ls
+from l.core import _INCLUDING_DOT_AND_DOTDOT, show
 from l.project import project
 
 
@@ -40,31 +40,3 @@ def run(all, paths, stdout=stdout):
     """
 
     stdout.write(show(paths=paths or (project("."),)))
-
-
-def show(paths):
-    output = (_unhidden(ls(path=path)) for path in paths)
-    if len(paths) > 1:
-        output = _labelled(
-            sorted(zip(paths, output), key=lambda (path, _) : path.path)
-        )
-    return "\n".join(output)
-
-
-def _labelled(parents_and_children):
-    return (
-        "{parent.path}:\n{children}".format(parent=parent, children=children)
-        for parent, children in parents_and_children
-    )
-
-
-def _unhidden(children):
-    return _formatted_children(
-        children=(
-            child for child in children if not child.basename().startswith(".")
-        )
-    )
-
-
-def _formatted_children(children):
-    return "  ".join(sorted(path.basename() for path in children)) + "\n"
--- l/tests/test_core.py
+++ l/tests/test_core.py
@@ -1,8 +1,9 @@
+from textwrap import dedent
 from unittest import TestCase
 
 from bp.memory import MemoryFS, MemoryPath
 
-from l.core import ls
+from l.core import ls, show
 
 
 class TestLS(TestCase):
@@ -19,3 +20,69 @@ class TestLS(TestCase):
         files = ls(path=self.root)
 
         self.assertEqual(files, [one, two])
+
+
+class TestShow(TestCase):
+    def setUp(self):
+        self.fs = MemoryFS()
+        self.root = MemoryPath(fs=self.fs, path=("test-dir",))
+        self.root.createDirectory()
+
+    def assertShows(self, result, **kwargs):
+        self.assertEqual(
+            show(**kwargs),
+            dedent(result).strip("\n") + "\n",
+        )
+
+    def children(self, *new, **kwargs):
+        of = kwargs.pop("of", self.root)
+        assert not kwargs
+
+        of.createDirectory()
+        for child in new:
+            path = of.child(child)
+            path.setContent("")
+            yield path
+
+    def test_it_lists_directories(self):
+        foo, bar = self.children("foo", "bar")
+        self.assertShows(paths=[self.root], result="bar  foo")
+
+    def test_it_lists_multiple_directories(self):
+        one = self.root.child("one")
+        two, four = self.children("two", "four", of=one)
+
+        three, = self.children("three")
+
+        self.assertShows(
+            paths=[self.root, one],
+            result="""
+            /mem/test-dir:
+            one  three
+
+            /mem/test-dir/one:
+            four  two
+            """,
+        )
+
+
+    def test_it_ignores_hidden_files_by_default(self):
+        foo, hidden = self.children("foo", ".hidden")
+        self.assertShows(paths=[self.root], result="foo")
+
+    def test_it_ignores_hidden_files_by_default_for_multiple_directories(self):
+        one = self.root.child("one")
+        two, four = self.children(".two", "four", of=one)
+
+        three, = self.children(".three")
+
+        self.assertShows(
+            paths=[self.root, one],
+            result="""
+            /mem/test-dir:
+            one
+
+            /mem/test-dir/one:
+            four
+            """,
+        )
